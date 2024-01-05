b207036597f27124f4393db852db8ada49ec0681
Vlad Brynzya <vladbrynzya@gmail.com>
2024-01-05 18:54:00 +0200

    fix db

--- config/database.js
+++ config/database.js
@@ -100,20 +100,20 @@ module.exports = ({ env }) => {
   };
 };
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "example"),
-      user: env("DATABASE_USERNAME", "example"),
-      password: env("DATABASE_PASSWORD", "example"),
-      ssl: env.bool("DATABASE_SSL", false),
-    },
-    debug: false,
-  },
-});
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "example"),
+//       user: env("DATABASE_USERNAME", "example"),
+//       password: env("DATABASE_PASSWORD", "example"),
+//       ssl: env.bool("DATABASE_SSL", false),
+//     },
+//     debug: false,
+//   },
+// });
 
 // const path = require("path");
 
